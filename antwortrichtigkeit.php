1e76ec1c1c41334515a88b639bd893ba870a58b8
Enrico Stemmer <100319043+H3rmt@users.noreply.github.com>
2022-03-04 19:35:24 +0100

    Update antwortrichtigkeit.php

--- antwortrichtigkeit.php
+++ antwortrichtigkeit.php
@@ -1,10 +1,11 @@
 <?php
 include "./database.php";
+
 $connection = db_connection_open();
-$sqlquery = $connection->prepare("SELECT antwort from `antworten` WHERE richtig = 1 AND fragenid = ?");
-$sqlquery->bind_param("s", $_GET["id"]);
-$sqlquery->execute();
-$sqlquery->bind_result($richtig);
-$sqlquery->fetch();
-echo $richtig;
+$sqlquery = $connection->prepare("SELECT antwort from `antworten` WHERE richtig = 1 AND fragenid = :id;");
+$sqlquery->execute([
+    ":id" => $_GET["id"]
+]);
+$arr = $sqlquery->fetchAll(PDO::FETCH_ASSOC)
+echo $arr;
 db_connection_close($connection);
